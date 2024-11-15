87f97a9c3f7cf73de4ce2bc347e939bfe1ee125e
RenataGodoy <devrenatagodoy@gmail.com>
2024-11-15 20:07:12 -0300

    try

--- public/index.php
+++ public/index.php
@@ -15,3 +15,9 @@ require __DIR__.'/../vendor/autoload.php';
 // Bootstrap Laravel and handle the request...
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
+
+
+//ports
+    $port = env('PORT', 9000);
+    $app->run($port, '0.0.0.0');
+    
